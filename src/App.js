5ba0480a515a3df6f3176be7000ec9cc452f620a
unknown <kalossi@gmail.com>
2024-04-18 15:16:13 +0300

    added fetch api but cors fail

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
-import Content from './Content';
-import Header from './Header';
-import Footer from './Footer'
-import { useState, useEffect } from 'react';
+import React from "react";
+import Content from "./Content";
+import Header from "./Header";
+import Footer from "./Footer";
+import { useState, useEffect } from "react";
 
 const App = () => {
-  const[pets, setPets] = useState([]);
+  const [pets, setPets] = useState([]);
+
+  const fetchPets = async () => {
+    const apiUrl = 'https://koira-api.herokuapp.com/api/v1/dogs';
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    pets = setPets(data);
+  }
 
   useEffect(() => {
-    console.log('Komponentti rendattu yhden kerran!')
-  },[])
+    fetchPets();
+  }, []);
+
+  useEffect(() => {
+    console.log(`pets: ${pets}`);
+  },[pets]);
 
   return (
     <div className="App">
-      <Header title="Nuuhkukuonot propseina"/>
-      <Content/>
-      <Footer/>
+      <Header title="Nuuhkukuonot propseina" />
+      <Content />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
