79b23266c7889d33f79e21df26753afb29879376
unknown <kalossi@gmail.com>
2024-04-23 15:22:32 +0300

    new branch for express tests

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import Content from "./Content";
 import Header from "./Header";
 import Footer from "./Footer";
 import { useState, useEffect } from "react";
+import * as express from "express";
+
+const app = express();
 
 const App = () => {
   const [pets, setPets] = useState([]);
 
-  const fetchPets = async () => {
-    const apiUrl = "https://api.thecatapi.com/api/v1/images/search?limit=10&api_key=live_0eg9nShpvS1uuFP6tPLKGqrs9ct5Lg05ZjXNqz7YJF8pMjeiGjxJChElRDgNVl7t";
+  app.get('/', async (req, res) => {
+    const apiUrl = "https://api.thecatapi.com/v1/images/search?limit=10&api_key=live_0eg9nShpvS1uuFP6tPLKGqrs9ct5Lg05ZjXNqz7YJF8pMjeiGjxJChElRDgNVl7t";
     try {
       const response = await fetch(apiUrl, {mode: 'no-cors'});
       const data = await response.json();
+      pets = res.json(data);
       pets = setPets(data);
     } catch (error) {
       console.log(error);
     }
-  };
+  });
 
   useEffect(() => {
     fetchPets();
